6fcf384cbdc5ec781b472b734ff157f27b871218
ikbalhussa1n <iqbalhussa1n@yahoo.com>
2026-02-16 23:15:06 -0500

    fetch all blogs, fetch single blogs or fetch only users blog
    implemented function to fetch all blogs,
    implemented function to single blog or
    only users blog

--- backend/routes/blog.route.js
+++ backend/routes/blog.route.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { upload } from "../middlewares/multer.js";
-import { createBlog } from "../controller/blog.controller.js";
+import {
+  createBlog,
+  deleteBlog,
+  findALlBlogs,
+  getMyBlogs,
+  singleBlog,
+} from "../controller/blog.controller.js";
 
 import { isUser } from "../middlewares/userAuth.js";
 
 const router = express.Router();
 
 router.post("/create", upload.single("imageBlog"), isUser, createBlog);
+router.delete("/delete/:id", isUser, deleteBlog);
+router.get("/all", isUser, findALlBlogs);
+router.get("/myBlogs", isUser, getMyBlogs);
+router.get("/singleBlog/:id", isUser, singleBlog);
 
 export default router;
